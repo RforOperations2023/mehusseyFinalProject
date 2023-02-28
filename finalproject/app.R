f5b476d7cfa1c6bb114295ad213ea7ebe6dc19f2
Megan Hussey <mehussey@andrew.cmu.edu>
2023-02-28 14:26:35 -0500

    Got my map to run on an app but not sure if the observe functions are working like I want them to.

--- finalproject/app.R
+++ finalproject/app.R
@@ -21,6 +21,10 @@ nypd <- read_csv("NYPD_Shooting_Incident_Data__Historic_.csv",
 nypd <- nypd %>%
   mutate(OCCUR_YEAR = year(ymd(OCCUR_DATE))) #creating a year only column 
 
+nypd_sf <- st_as_sf(nypd, coords = c("Longitude", "Latitude"), crs = 4326)
+joined_data <- st_join(nypd_sf, boro_shapes, join = st_within)
+
+
 # Define colors for each boro
 boro_colors <- c("Brooklyn" = "#FF0000", "Queens" = "#00FF00", 
                  "Manhatten" = "#0000FF", "Bronx" = "#FFA500", 
@@ -38,8 +42,9 @@ ui <- fluidPage(
       selectInput(inputId = "year", 
                   label = "Select a Year", 
                   choices = c(2019, 2020, 2021), 
-                  selected = 2021),
-      selectInput(inputId = "muder",
+                  selected = 2021,
+                  multiple = TRUE),
+      selectInput(inputId = "death",
                   label = "Specify if the shooting was fatal or not:",
                   choices = unique(sort(nypd$STATISTICAL_MURDER_FLAG)),
                   selected = NA, 
@@ -54,22 +59,39 @@ ui <- fluidPage(
 # Define server
 server <- function(input, output) {
   
-  filtered_nypd <- reactiveVal(nypd)
-  
-  observe({
-    filtered_nypd(nypd %>% filter(OCCUR_YEAR == input$year & boro_name %in% input$boro))
+  #Main reactive function to change with each input
+  filtered_nypd <- reactive({
+    req(input$boro, input$year, input$death)
+    joined_data %>% 
+      filter(OCCUR_YEAR == input$year & boro_name %in% input$boro 
+             & STATISTICAL_MURDER_FLAG == input$death)
   })
   
-  output$nypd_map <- renderLeaflet({
+  #Reactive function for polygon layers
+  filtered_boro <- reactive({
+    req(input$boro)
+    filter(boro_shapes, boro_name %in% input$boro)
+  })
+  
+#MAIN LEAFLET MAP WITH CLUSTERS
+  output$map <- renderLeaflet({
     leaflet() %>%
       addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", 
                attribution = "Google", group = "Google") %>%  
       addProviderTiles(provider = providers$Wikimedia, group = "Wiki") %>%
       setView(-74.0060, 40.7128, 9) %>%
-      addPolygons(data = boro_shapes, color = ~boro_colors, group = "boro_name") %>%
       addMarkers(data = filtered_nypd(), clusterOptions = markerClusterOptions())
   })
 }
+
+#CREATING POLYGON LAYERS WITH BOROUGHS
+observe({
+  boroInf <- filtered_boro()
+  leafletProxy("map", data = boroInf) %>%
+    clearGroup(group = "Boroughs") %>%
+    addPolygons(popup = ~paste0("<b>", LABEL, "</b>"), group = "Boroughs", layerId = ~boro_names, fill = FALSE, color = ~boro_colors)
+})
+
 
 # Run the app
 shinyApp(ui = ui, server = server)
